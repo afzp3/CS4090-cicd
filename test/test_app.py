bb4d48eac1264c333ab5622563fba4a0c803240b
Abigail Fletcher <146607926+afzp3@users.noreply.github.com>
2025-12-04 18:11:39 +0000

    test_app.py

--- test/test_app.py
+++ test/test_app.py
@@ -1,8 +1,11 @@
-from src.app import add
-from pathlib import pathlib
+import sys
+from pathlib import Path
 
-root = Path(__file__.resolve().parents[1])
+root = Path(__file__).resolve().parents[1]
 sys.path.insert(0, str(root / "src"))
+
+
+from app import add
 
 def test_add():
     assert add(5, 6) == 11
