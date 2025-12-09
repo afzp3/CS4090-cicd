c86b0820c247dea917b3e94ddc0b7347889bd8da
Abigail Fletcher <146607926+afzp3@users.noreply.github.com>
2025-12-09 07:33:34 +0000

    add more functionalities with test cases

--- src/app.py
+++ src/app.py
@@ -1,2 +1,17 @@
 def add(a, b):
     return a + b
+
+def sub(a, b):
+    return a - b
+
+def mult(a, b):
+    return a * b
+
+def div(a, b):
+    try:
+        return a / b
+    except:
+        return ZeroDivisionError
+
+def squared(a):
+    return a ** 2
--- test/test_app.py
+++ test/test_app.py
@@ -5,10 +5,40 @@ root = Path(__file__).resolve().parents[1]
 sys.path.insert(0, str(root / "src"))
 
 
-from app import add
+from app import add, sub, mult, div, squared
+import pytest
 
 def test_add():
     assert add(5, 6) == 11
 
 def test_add2():
     assert add(5, 6) != 10
+
+def test_sub1():
+    assert sub(6, 5) == -sub(5, 6)
+
+def test_sub2():
+    assert sub(10, 10) == 0
+
+@pytest.mark.parametrize("a", [1, 2, 3, 4, 5])
+def test_mult_identity(a):
+    assert mult(a, 1) == a
+
+@pytest.mark.parametrize("x,y", [(1, 2), (2, 3), (3, 4), (4, 5), (5, 0)])
+def test_mult_commutative(x, y):
+    assert mult(x, y) == mult(y, x)
+
+@pytest.mark.parametrize(
+    "x,y,expected",
+    [
+        (10,1,10),
+        (1.2,2,0.6),
+        (-1,0,ZeroDivisionError)
+    ]
+)
+def test_div(x,y,expected):
+    assert div(x, y) == expected
+
+@pytest.mark.parametrize("a", [1, 2, 3, 4, 5])
+def test_squared(a):
+    assert squared(a) == mult(a, a)
