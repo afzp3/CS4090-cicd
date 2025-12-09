6dfacfaad57052899a2db2aebbfa51919651c53f
Abigail Fletcher <146607926+afzp3@users.noreply.github.com>
2025-12-09 19:23:35 +0000

    more functions

--- src/app.py
+++ src/app.py
@@ -1,3 +1,5 @@
+from math import sqrt, log, sin, cos
+
 def add(a, b):
     return a + b
 
@@ -11,7 +13,35 @@ def div(a, b):
     try:
         return a / b
     except:
-        return ZeroDivisionError
+        raise ZeroDivisionError("Cannot divide by zero")
+
+def log(a, base=10):
+    if a <= 0:
+        raise ValueError("Log is undefined for zero or negative numbers")
+    if base <= 0 or base == 1:
+        raise ValueError("Logarithm base must be positive and not equal to 1.")
+    return math.log(a, base)
 
 def squared(a):
     return a ** 2
+
+def sin(a, in_degrees=False):
+    if in_degrees:
+        a = math.radians(a)
+    return math.sin(a)
+
+def cos(a, in_degrees=False):
+    if in_degrees:
+        a = math.radians(a)
+    return math.cos(a)
+
+def squareroot(a):
+    if a < 0:
+        raise ValueError("Cannot take square root of negative number")
+    return sqrt(a)
+
+def percentage(a, b):
+    # a is x% of b
+    if b == 0:
+        raise ValueError("Cannot divide by zero.")
+    return (a / b) * 100
--- test/test_app.py
+++ test/test_app.py
@@ -4,9 +4,9 @@ from pathlib import Path
 root = Path(__file__).resolve().parents[1]
 sys.path.insert(0, str(root / "src"))
 
-
-from app import add, sub, mult, div, squared
+from app import add, sub, mult, div, squared, log, sin, cos, percentage, squareroot
 import pytest
+from math import sqrt, log, sin, cos
 
 def test_add():
     assert add(5, 6) == 11
@@ -37,8 +37,61 @@ def test_mult_commutative(x, y):
     ]
 )
 def test_div(x,y,expected):
-    assert div(x, y) == expected
+    if expected == ZeroDivisionError:
+        with pytest.raises(ZeroDivisionError):
+            div(x,y)
+    else:
+        assert div(x, y) == expected
 
 @pytest.mark.parametrize("a", [1, 2, 3, 4, 5])
 def test_squared(a):
     assert squared(a) == mult(a, a)
+
+@pytest.mark.parametrize("base", [2, 3, 4, 5])
+def test_log1(base):
+    # log rule: log_b(1) = 0
+    assert log(1, base) == 0
+
+@pytest.mark.parametrize("base", [2, 3, 4, 5])
+def test_log2(base):
+    # log rule: log_b(b) = 1
+    assert log(base, base) == 1
+
+@pytest.mark.parametrize("a", [1, 2, 3, 4, 5])
+def test_sin(a):
+    assert sin(a) >= -1 and sin(a) <= 1
+
+@pytest.mark.parametrize("a", [1, 2, 3, 4, 5])
+def test_cos(a):
+    assert cos(a) >= -1 and cos(a) <= 1
+
+@pytest.mark.parametrize(
+    "x,y,expected",
+    [
+        (1,10,10),
+        (6,6,100),
+        (1,0,ValueError)
+    ]
+)
+def test_percentage(x, y, expected):
+    if expected == ValueError:
+        with pytest.raises(ValueError):
+            percentage(x, y)
+    else:
+        assert percentage(x, y) == expected
+
+@pytest.mark.parametrize(
+    "x,expected",
+    [
+        (49,7),
+        (36,6),
+        (0, 0),
+        (-1,ValueError)
+    ]
+)
+def test_squareroot(x, expected):
+    if expected == ValueError:
+        with pytest.raises(ValueError):
+            squareroot(x)
+    else:
+        assert squareroot(x) == expected
